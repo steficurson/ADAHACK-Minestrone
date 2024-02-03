2834cde4e31f6b7e279398b504b0298c418ff2ed
steficurson <s2172960@ed.ac.uk>
2024-02-03 14:48:36 +0000

    fix

--- PastPapers/app/controllers/sessions_controller.rb
+++ PastPapers/app/controllers/sessions_controller.rb
@@ -6,10 +6,16 @@ class SessionsController < ApplicationController
     if @user && @user.authenticate(params[:password])
       #Success: log the user in
       session[:user_id] = @user.id
-      redirect_to user_path(@user)
+      redirect_to home_path
     else
       message = "Invalid email and/or password. Please try again."
       redirect_to login_path, notice: message
     end
+  end
+
+  def destroy
+    #Log the user out
+    session[:user_id] = nil
+    redirect_to home_path
   end
 end
